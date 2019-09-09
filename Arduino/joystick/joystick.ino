90ae51fd9a242263034fbe4dea4de8838e80cf4f
lorenzorivosecchi <lorenzorivosecchi3@gmail.com>
2019-09-09 18:47:47 +0200

    ADDED led smooth blinking

--- Arduino/joystick/joystick.ino
+++ Arduino/joystick/joystick.ino
@@ -22,11 +22,8 @@ int x_at_rest;
 int y_at_rest;
 
 // Led stuff
-int led_state = true;
-int blink_interval = 1000;
-int blink_time = 0;
-int next_blink = 0;
-int reset_blink = false;
+int led_brightness;
+int led_counter = 0;
 
 // Program flow
 boolean first_loop = true;
@@ -40,7 +37,6 @@ void setup() {
   pinMode(SW_pin, INPUT);
   digitalWrite(SW_pin, HIGH);
   pinMode(LED_pin, OUTPUT);
-  digitalWrite(LED_pin, HIGH);
 
   // Calculate rest values for sensor 
   x_at_rest = 512 - analogRead(X_pin);
@@ -76,7 +72,7 @@ void loop() {
   ty = constrain(ty, -512, 512);
 
   // If values have changed significantly:
-  if (valuesChanged(tx, ty, sw) || first_loop) { 
+  if (values_changed(tx, ty, sw) || first_loop) { 
     // Override memorized values
     last_x = tx;
     last_y = ty;
@@ -92,29 +88,14 @@ void loop() {
     int dist = sqrt(tx^2 + ty^2);
     // Normalize it
     int ndist = dist / 512;
-    // Set blink interval from it
-    blink_interval = 100 + 1000 * ndist;
-    reset_blink = true;
-  } 
+  }
+
+  led_counter+= 20;
+  led_brightness = 40 + (0.5 + 0.5 * sin(radians(led_counter))) * 205;
+  analogWrite(LED_pin, led_brightness);
 
   first_loop = false;
   delay(100);
-}
-
-/**
- * TOGGLE LED
- * ==========
- * This function toggles the led indicator
- * from high to low state
- */
-void toggleLed() {
-  if (led_state == true) {
-    digitalWrite(LED_pin, LOW);
-    led_state = false;
-  } else {
-    digitalWrite(LED_pin, HIGH);
-    led_state = true;
-  }
 }
 
 /**
@@ -123,7 +104,7 @@ void toggleLed() {
  * This function returns true if the given x or y values
  * have changed significally from their recorded values
  */
-boolean valuesChanged(int x, int y, int sw) {
+boolean values_changed(int x, int y, int sw) {
   int minimum = 2; 
   return abs(x - last_x) >= minimum ||
          abs(y - last_y) >= minimum || 
