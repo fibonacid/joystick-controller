a35beda6986dd5484a63ab0a990904af72e7d3c5
lorenzorivosecchi <lorenzorivosecchi3@gmail.com>
2019-09-09 12:04:34 +0200

    CHANGED coordinate system of the sensor

--- Arduino/joystick/joystick.ino
+++ Arduino/joystick/joystick.ino
@@ -12,19 +12,124 @@
 const int SW_pin = 2;
 const int X_pin = 0;
 const int Y_pin = 1;
+const int LED_pin = 3;
 
+// Register
+int last_sw;
+int last_x;
+int last_y;
+int x_at_rest;
+int y_at_rest;
+
+// Led stuff
+int led_state = true;
+int blink_interval = 1000;
+int blink_time = 0;
+int next_blink = 0;
+int reset_blink = false;
+
+// Program flow
+boolean first_loop = true;
+
+/**
+ * SETUP 
+ * =====
+ */
 void setup() {
+  // Initialize pins
   pinMode(SW_pin, INPUT);
   digitalWrite(SW_pin, HIGH);
+  pinMode(LED_pin, OUTPUT);
+  digitalWrite(LED_pin, HIGH);
+
+  // Calculate rest values for sensor 
+  x_at_rest = 512 - analogRead(X_pin);
+  y_at_rest = 512 - analogRead(Y_pin);
+
+  // Start serial comunication
   Serial.begin(115200);
+  Serial.print("\n");
+  // The receiver should wait for this message
+  Serial.print("begin\n"); 
 }
 
+/**
+ * LOOP
+ * ====
+ */
 void loop() {
-  Serial.print(analogRead(X_pin));
-  Serial.print(",");
-  Serial.print(analogRead(Y_pin));
-  Serial.print(",");
-  Serial.print(!digitalRead(SW_pin));
-  Serial.print("\n");
-  delay(100);
+
+  // Get time
+  int current_time = millis();
+  
+  // Get sensor data
+  int x = analogRead(X_pin);
+  int y = analogRead(Y_pin);
+  int sw = !digitalRead(SW_pin);
+
+  // Apply transformations
+  int tx = 512 - y;
+  int ty = 512 - x;
+
+  // Fix precision
+  tx -= y_at_rest;
+  ty -= x_at_rest;
+
+  // Protect from exceeding the nominal bounds.
+  tx = constrain(tx, -512, 512);
+  ty = constrain(ty, -512, 512);
+
+  // If values have changed significantly:
+  if (valuesChanged(tx, ty, sw) || first_loop) { 
+    // Override memorized values
+    last_x = tx;
+    last_y = ty;
+    last_sw = sw;
+    // Send data
+    Serial.print(tx);
+    Serial.print(",");
+    Serial.print(ty);
+    Serial.print(",");
+    Serial.print(sw);
+    Serial.print("\n");
+    // Calculate distance from center
+    int dist = sqrt(tx^2 + ty^2);
+    // Normalize it
+    int ndist = dist / 512;
+    // Set blink interval from it
+    blink_interval = 100 + 1000 * ndist;
+    reset_blink = true;
+  } 
+
+  first_loop = false;
+  delay(50);
+}
+
+/**
+ * TOGGLE LED
+ * ==========
+ * This function toggles the led indicator
+ * from high to low state
+ */
+void toggleLed() {
+  if (led_state == true) {
+    digitalWrite(LED_pin, LOW);
+    led_state = false;
+  } else {
+    digitalWrite(LED_pin, HIGH);
+    led_state = true;
+  }
+}
+
+/**
+ * VALUES CHANGED
+ * ==============
+ * This function returns true if the given x or y values
+ * have changed significally from their recorded values
+ */
+boolean valuesChanged(int x, int y, int sw) {
+  int minimum = 2; 
+  return abs(x - last_x) >= minimum ||
+         abs(y - last_y) >= minimum || 
+         sw != last_sw;
 }
